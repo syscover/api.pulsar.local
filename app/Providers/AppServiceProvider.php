b646447e756df7f6db6e5ef889872a67aa418d03
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-04-26 18:59:44 +0200

    minor fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        DB::listen(function ($query) {
+            // $query->sql
+            // $query->bindings
+            // $query->time
+
+           // Log::info($query->sql);
+        });
     }
 
     /**
