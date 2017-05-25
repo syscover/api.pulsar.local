8815c69a98937260b13ef98ee78fb7f64abf14d1
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-05-25 13:10:51 +0200

    404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('web.layouts.default')
+
+@section('title', 'Home')
+
+@section('head')
+@stop
+
+@section('content')
+    {{ user_lang() }}
+    <div class="content">
+        <div class="title">404 Be right back.</div>
+    </div>
+@stop
