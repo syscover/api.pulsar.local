7f982139240718e29ed826e22aa75d6e05bdbf8e
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-08-19 11:25:31 +0200

    refactor routes

--- routes/web.php
+++ routes/web.php
@@ -2,78 +2,78 @@
 
 Route::group(['middleware' => ['pulsar.navtools']], function () {
 
-    Route::get('/',                                                                         ['as' => 'home',                                'uses' => '\App\Http\Controllers\WebFrontendController@home']);
-    Route::get('/es',                                                                       ['as' => 'home-es',                             'uses' => '\App\Http\Controllers\WebFrontendController@home']);
-    Route::get('/en',                                                                       ['as' => 'home-en',                             'uses' => '\App\Http\Controllers\WebFrontendController@home']);
+    Route::get('/',                                                                         '\App\Http\Controllers\WebFrontendController@home')->name('home');
+    Route::get('/es',                                                                       '\App\Http\Controllers\WebFrontendController@home')->name('home-es');
+    Route::get('/en',                                                                       '\App\Http\Controllers\WebFrontendController@home')->name('home-en');
 
     // CMS - BLOG
     // EN
-    Route::get('/en/blog',                                                                  ['as' => 'blog-en',                             'uses' => '\App\Http\Controllers\BlogFrontendController@getBlog']);
-    Route::get('/en/blog/{slug}',                                                           ['as' => 'article-en',                          'uses' => '\App\Http\Controllers\BlogFrontendController@getArticle']);
+    Route::get('/en/blog',                                                                  '\App\Http\Controllers\BlogFrontendController@getBlog')->name('blog-en');
+    Route::get('/en/blog/{slug}',                                                           '\App\Http\Controllers\BlogFrontendController@getArticle')->name('article-en');
 
     // ES
-    Route::get('/es/blog',                                                                  ['as' => 'blog-es',                             'uses' => '\App\Http\Controllers\BlogFrontendController@getBlog']);
-    Route::get('/es/blog/{slug}',                                                           ['as' => 'article-es',                          'uses' => '\App\Http\Controllers\BlogFrontendController@getArticle']);
+    Route::get('/es/blog',                                                                  '\App\Http\Controllers\BlogFrontendController@getBlog')->name('blog-es');
+    Route::get('/es/blog/{slug}',                                                           '\App\Http\Controllers\BlogFrontendController@getArticle')->name('article-es');
 
 
     // CUSTOMER ACCOUNT
     // EN
-    Route::get('/en/account/login',                                                         ['as' => 'getLogin-en',                         'uses' => '\App\Http\Controllers\CustomerFrontendController@getLogin']);
-    Route::get('/en/account/sing-in',                                                       ['as' => 'getSingIn-en',                        'uses' => '\App\Http\Controllers\CustomerFrontendController@getSingIn']);
-    Route::post('/en/account/sing-in',                                                      ['as' => 'postSingIn-en',                       'uses' => '\App\Http\Controllers\CustomerFrontendController@postSingIn']);
+    Route::get('/en/account/login',                                                         '\App\Http\Controllers\CustomerFrontendController@getLogin')->name('getLogin-en');
+    Route::get('/en/account/sing-in',                                                       '\App\Http\Controllers\CustomerFrontendController@getSingIn')->name('getSingIn-en');
+    Route::post('/en/account/sing-in',                                                      '\App\Http\Controllers\CustomerFrontendController@postSingIn')->name('postSingIn-en');
 
     // ES
-    Route::get('/es/cuenta/login',                                                          ['as' => 'getLogin-es',                         'uses' => '\App\Http\Controllers\CustomerFrontendController@getLogin']);
-    Route::get('/es/cuenta/registro',                                                       ['as' => 'getSingIn-es',                        'uses' => '\App\Http\Controllers\CustomerFrontendController@getSingIn']);
-    Route::post('/es/cuenta/registro',                                                      ['as' => 'postSingIn-es',                       'uses' => '\App\Http\Controllers\CustomerFrontendController@postSingIn']);
+    Route::get('/es/cuenta/login',                                                          '\App\Http\Controllers\CustomerFrontendController@getLogin')->name('getLogin-es');
+    Route::get('/es/cuenta/registro',                                                       '\App\Http\Controllers\CustomerFrontendController@getSingIn')->name('getSingIn-es');
+    Route::post('/es/cuenta/registro',                                                      '\App\Http\Controllers\CustomerFrontendController@postSingIn')->name('postSingIn-es');
 
-    Route::post('/account/login',                                                           ['as' => 'postLogin',                           'uses' => '\App\Http\Controllers\CustomerFrontendController@postLogin']);
+    Route::post('/account/login',                                                           '\App\Http\Controllers\CustomerFrontendController@postLogin')->name('postLogin');
 
     // SHOPPING CART
     // EN
-    Route::get('/en/shopping/cart',                                                         ['as' => 'getShoppingCart-en',                  'uses' => '\App\Http\Controllers\ShoppingCartController@getShoppingCart']);
-    Route::match(['get', 'post'], '/en/shopping/cart/add/product/{slug}',                   ['as' => 'addProduct-en',                       'uses' => '\App\Http\Controllers\ShoppingCartController@addProduct']);
-    Route::match(['get', 'post'], '/en/shopping/cart/delete/product/{rowId}',               ['as' => 'deleteProduct-en',                    'uses' => '\App\Http\Controllers\ShoppingCartController@deleteProduct']);
-    Route::put('/en/shopping/cart/update',                                                  ['as' => 'updateProduct-en',                    'uses' => '\App\Http\Controllers\ShoppingCartController@updateProduct']);
-    Route::post('/en/shopping/cart/check/coupon/code',                                      ['as' => 'checkCouponCode-en',                  'uses' => '\App\Http\Controllers\ShoppingCartController@checkCouponCode']);
+    Route::get('/en/shopping/cart',                                                         '\App\Http\Controllers\ShoppingCartController@getShoppingCart')->name('getShoppingCart-en');
+    Route::match(['get', 'post'], '/en/shopping/cart/add/product/{slug}',                   '\App\Http\Controllers\ShoppingCartController@addProduct')->name('addProduct-en');
+    Route::match(['get', 'post'], '/en/shopping/cart/delete/product/{rowId}',               '\App\Http\Controllers\ShoppingCartController@deleteProduct')->name('deleteProduct-en');
+    Route::put('/en/shopping/cart/update',                                                  '\App\Http\Controllers\ShoppingCartController@updateProduct')->name('updateProduct-en');
+    Route::post('/en/shopping/cart/check/coupon/code',                                      '\App\Http\Controllers\ShoppingCartController@checkCouponCode')->name('checkCouponCode-en');
 
     // ES
-    Route::get('/es/carro-compra',                                                          ['as' => 'getShoppingCart-es',                  'uses' => '\App\Http\Controllers\ShoppingCartController@getShoppingCart']);
-    Route::match(['get', 'post'], '/es/carro-compra/anadir-producto/{slug}',                ['as' => 'addProduct-es',                       'uses' => '\App\Http\Controllers\ShoppingCartController@addProduct']);
-    Route::match(['get', 'post'], '/es/carro-compra/borrar-producto/{rowId}',               ['as' => 'deleteProduct-es',                    'uses' => '\App\Http\Controllers\ShoppingCartController@deleteProduct']);
-    Route::put('/es/carro-compra/actualizar',                                               ['as' => 'updateProduct-es',                    'uses' => '\App\Http\Controllers\ShoppingCartController@updateProduct']);
-    Route::post('/es/carro-compra/comprueba/codigo/cupon',                                  ['as' => 'checkCouponCode-es',                  'uses' => '\App\Http\Controllers\ShoppingCartController@checkCouponCode']);
+    Route::get('/es/carro-compra',                                                          '\App\Http\Controllers\ShoppingCartController@getShoppingCart')->name('getShoppingCart-es');
+    Route::match(['get', 'post'], '/es/carro-compra/anadir-producto/{slug}',                '\App\Http\Controllers\ShoppingCartController@addProduct')->name('addProduct-es');
+    Route::match(['get', 'post'], '/es/carro-compra/borrar-producto/{rowId}',               '\App\Http\Controllers\ShoppingCartController@deleteProduct')->name('deleteProduct-es');
+    Route::put('/es/carro-compra/actualizar',                                               '\App\Http\Controllers\ShoppingCartController@updateProduct')->name('updateProduct-es');
+    Route::post('/es/carro-compra/comprueba/codigo/cupon',                                  '\App\Http\Controllers\ShoppingCartController@checkCouponCode')->name('checkCouponCode-es');
 });
 
 Route::group(['middleware' => ['pulsar.navtools', 'pulsar.crm.auth']], function() {
 
     // CUSTOMER ACCOUNT
     // EN
-    Route::match(['get', 'post'], '/en/account',                                            ['as' => 'account-en',                          'uses' => '\App\Http\Controllers\CustomerFrontendController@account']);
-    Route::put('/en/account/sing-in',                                                       ['as' => 'putSingIn-en',                        'uses' => '\App\Http\Controllers\CustomerFrontendController@putSingIn']);
-    Route::match(['get', 'post'], '/en/account/logout',                                     ['as' => 'logout-en',                           'uses' => '\App\Http\Controllers\CustomerFrontendController@logout']);
+    Route::match(['get', 'post'], '/en/account',                                            '\App\Http\Controllers\CustomerFrontendController@account')->name('account-en');
+    Route::put('/en/account/sing-in',                                                       '\App\Http\Controllers\CustomerFrontendController@putSingIn')->name('putSingIn-en');
+    Route::match(['get', 'post'], '/en/account/logout',                                     '\App\Http\Controllers\CustomerFrontendController@logout')->name('logout-en');
 
     // ES
-    Route::match(['get', 'post'], '/es/cuenta',                                             ['as' => 'account-es',                          'uses' => '\App\Http\Controllers\CustomerFrontendController@account']);
-    Route::put('/es/cuenta/registro',                                                       ['as' => 'putSingIn-es',                        'uses' => '\App\Http\Controllers\CustomerFrontendController@putSingIn']);
-    Route::match(['get', 'post'], '/es/cuenta/logout',                                      ['as' => 'logout-es',                           'uses' => '\App\Http\Controllers\CustomerFrontendController@logout']);
+    Route::match(['get', 'post'], '/es/cuenta',                                             '\App\Http\Controllers\CustomerFrontendController@account')->name('account-es');
+    Route::put('/es/cuenta/registro',                                                       '\App\Http\Controllers\CustomerFrontendController@putSingIn')->name('putSingIn-es');
+    Route::match(['get', 'post'], '/es/cuenta/logout',                                      '\App\Http\Controllers\CustomerFrontendController@logout')->name('logout-es');
 
     // CHERCKOUT
     // EN
-    Route::get('/en/checkout/shipping',                                                     ['as' => 'getCheckout01-en',            'uses' => '\App\Http\Controllers\MarketFrontendController@getCheckout01']);
-    Route::post('/en/checkout/shipping',                                                    ['as' => 'postCheckout01-en',           'uses' => '\App\Http\Controllers\MarketFrontendController@postCheckout01']);
-    Route::get('/en/checkout/invoice',                                                      ['as' => 'getCheckout02-en',            'uses' => '\App\Http\Controllers\MarketFrontendController@getCheckout02']);
-    Route::post('/en/checkout/invoice',                                                     ['as' => 'postCheckout02-en',           'uses' => '\App\Http\Controllers\MarketFrontendController@postCheckout02']);
-    Route::get('/en/checkout/payment',                                                      ['as' => 'getCheckout03-en',            'uses' => '\App\Http\Controllers\MarketFrontendController@getCheckout03']);
-    Route::post('/en/checkout/payment',                                                     ['as' => 'postCheckout03-en',           'uses' => '\App\Http\Controllers\MarketFrontendController@postCheckout03']);
+    Route::get('/en/checkout/shipping',                                                     '\App\Http\Controllers\MarketFrontendController@getCheckout01')->name('getCheckout01-en');
+    Route::post('/en/checkout/shipping',                                                    '\App\Http\Controllers\MarketFrontendController@postCheckout01')->name('postCheckout01-en');
+    Route::get('/en/checkout/invoice',                                                      '\App\Http\Controllers\MarketFrontendController@getCheckout02')->name('getCheckout02-en');
+    Route::post('/en/checkout/invoice',                                                     '\App\Http\Controllers\MarketFrontendController@postCheckout02')->name('postCheckout02-en');
+    Route::get('/en/checkout/payment',                                                      '\App\Http\Controllers\MarketFrontendController@getCheckout03')->name('getCheckout03-en');
+    Route::post('/en/checkout/payment',                                                     '\App\Http\Controllers\MarketFrontendController@postCheckout03')->name('postCheckout03-en');
 
     // ES
-    Route::get('/es/realizar/pedido/envio',                                                 ['as' => 'getCheckout01-es',            'uses' => '\App\Http\Controllers\MarketFrontendController@getCheckout01']);
-    Route::post('/es/realizar/pedido/envio',                                                ['as' => 'postCheckout01-es',           'uses' => '\App\Http\Controllers\MarketFrontendController@postCheckout01']);
-    Route::get('/es/realizar/pedido/factura',                                               ['as' => 'getCheckout02-es',            'uses' => '\App\Http\Controllers\MarketFrontendController@getCheckout02']);
-    Route::post('/es/realizar/pedido/factura',                                              ['as' => 'postCheckout02-es',           'uses' => '\App\Http\Controllers\MarketFrontendController@postCheckout02']);
-    Route::get('/es/realizar/pedido/pago',                                                  ['as' => 'getCheckout03-es',            'uses' => '\App\Http\Controllers\MarketFrontendController@getCheckout03']);
-    Route::post('/es/realizar/pedido/pago',                                                 ['as' => 'postCheckout03-es',           'uses' => '\App\Http\Controllers\MarketFrontendController@postCheckout03']);
+    Route::get('/es/realizar/pedido/envio',                                                 '\App\Http\Controllers\MarketFrontendController@getCheckout01')->name('getCheckout01-es');
+    Route::post('/es/realizar/pedido/envio',                                                '\App\Http\Controllers\MarketFrontendController@postCheckout01')->name('postCheckout01-es');
+    Route::get('/es/realizar/pedido/factura',                                               '\App\Http\Controllers\MarketFrontendController@getCheckout02')->name('getCheckout02-es');
+    Route::post('/es/realizar/pedido/factura',                                              '\App\Http\Controllers\MarketFrontendController@postCheckout02')->name('postCheckout02-es');
+    Route::get('/es/realizar/pedido/pago',                                                  '\App\Http\Controllers\MarketFrontendController@getCheckout03')->name('getCheckout03-es');
+    Route::post('/es/realizar/pedido/pago',                                                 '\App\Http\Controllers\MarketFrontendController@postCheckout03')->name('postCheckout03-es');
 
 });
 
