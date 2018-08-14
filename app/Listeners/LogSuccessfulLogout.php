9b9bf165a029a1ef808e4f6f887a4d84d960edfc
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-08-14 16:43:08 +0200

    minor fix

--- app/Listeners/LogSuccessfulLogout.php
+++ app/Listeners/LogSuccessfulLogout.php
@@ -1,12 +1,19 @@
 <?php namespace App\Listeners;
 
 use Illuminate\Auth\Events\Logout;
+use Syscover\Market\Services\TaxRuleService;
 
 class LogSuccessfulLogout
 {
     public function handle(Logout $event)
     {
+        // get default tax rules
+        $taxRules = TaxRuleService::getCustomerTaxRules();
+
+        // calculate prices in shopping cart with default values
+        TaxRuleService::taxCalculateOverShoppingCart($taxRules);
+
         // reset tax_rules session variable
-        session(['pulsar-market.tax_rules' => null]);
+        session(['pulsar-market.tax_rules' => $taxRules]);
     }
 }
