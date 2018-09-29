9d3725ac38f4dfb3f96393a8aafbe4a49b8d4152
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-09-29 12:48:09 +0200

    minor fix

--- app/Listeners/LogSuccessfulLogin.php
+++ app/Listeners/LogSuccessfulLogin.php
@@ -7,15 +7,18 @@ class LogSuccessfulLogin
 {
     public function handle(Login $event)
     {
-        // calculate tax rules for customer logged
-        TaxRuleService::checkCustomerTaxRules(
-            $event->user->class_tax->id ?? null,
-            $event->user->country_id,
-            $event->user->territorial_area_1_id,
-            $event->user->territorial_area_2_id,
-            $event->user->territorial_area_3_id,
-            $event->user->zip,
-            $event->guard
-        );
+        if($event->guard === 'crm')
+        {
+            // calculate tax rules for customer logged
+            TaxRuleService::checkCustomerTaxRules(
+                $event->user->class_tax->id ?? null,
+                $event->user->country_id,
+                $event->user->territorial_area_1_id,
+                $event->user->territorial_area_2_id,
+                $event->user->territorial_area_3_id,
+                $event->user->zip,
+                $event->guard
+            );
+        }
     }
 }
