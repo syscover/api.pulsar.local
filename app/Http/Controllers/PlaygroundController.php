fbdec5fa2ecb3b1a2197d673b9ac0e75f6b6cea4
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-03-10 11:49:21 +0100

    playground

--- app/Http/Controllers/PlaygroundController.php
+++ app/Http/Controllers/PlaygroundController.php
@@ -1,10 +1,13 @@
 <?php namespace App\Http\Controllers;
 
 
+use Syscover\Core\Services\SchemaService;
+
 class PlaygroundController extends Controller
 {
     public function index()
     {
+        // dd(SchemaService::hasIndex('admin_lang', 'ix01_admin_lang'));
 
         // dd(now()->toDateTimeString());
         // First we will see if we have a cache configuration file. If we do, we'll load
