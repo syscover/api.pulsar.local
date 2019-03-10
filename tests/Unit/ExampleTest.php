3bbd067e22c765b75dec6206736ec733f29ad343
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-03-10 23:12:40 +0100

    Testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,18 +2,63 @@
 
 namespace Tests\Unit;
 
+use Syscover\Admin\Models\Package;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    public static function setUpBeforeClass()
     {
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    protected function assertPreConditions()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    public function testOne()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
         $this->assertTrue(true);
+    }
+
+    public function testTwo()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+        $this->assertTrue(true);
+    }
+
+    protected function assertPostConditions()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    protected function tearDown(): void
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+
+        parent::tearDown();
+    }
+
+    public static function tearDownAfterClass()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+
+        parent::tearDownAfterClass();
+    }
+
+    protected function onNotSuccessfulTest(\Tests\Unit\Exception $e)
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+        throw $e;
     }
 }
